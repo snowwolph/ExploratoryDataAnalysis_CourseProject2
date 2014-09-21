028dc2b5d85e6f842d671d71bc608c4d9d27fd1b
snowwolph <snowwolph@gmail.com>
2014-09-21 11:59:01 -0400

    Plot6 Script
    Script creates a bar plot to answer: Compare emissions from motor
    vehicle sources in Baltimore City with emissions from motor vehicle
    sources in Los Angeles County, California ( fips == "06037"). Which city
    has seen greater changes over time in motor vehicle emissions?

---
+++ plot6.R
@@ -0,0 +1,87 @@
+################################################################################
+## plot6.R
+##
+## plot6.R - Creates a bar plot of 'Total PM[2.5] Emissions: 
+##                (Motor Vehicle), PM[2.5] Emissions vs. Years by 
+##                U.S County
+##
+##
+## Author - Gregg O'Marr
+## Date - 21 September 2014
+## Version - 1.1
+###############################################################################
+require( ggplot2 )      ## Plotting framework
+require( plyr )     ## Reshape the data with ddply()
+
+NEI <- readRDS( "./summarySCC_PM25.rds" )       ## Read-in data.frame
+
+## Reduce data by dropping unnecessary variables
+NEI_Reduced <- NEI[ , c( 'fips', 
+                         'Emissions', 
+                         'type', 
+                         'year' ) ]
+
+## Reduce data further by only selecting rows with Baltimore zipcode '24510' |
+##    '06037' and type == 'ON-ROAD'
+NEI_Baltimore <- NEI_Reduced[ 
+                        (NEI_Reduced[ , 'fips' ] == '06037' | NEI_Reduced[ , 'fips' ] == '24510') & NEI_Reduced[ , 'type' ] == 'ON-ROAD', 
+                        c( 'fips', 'Emissions', 'type', 'year' ) ]
+
+NEI_Baltimore$fips <- as.factor( NEI_Baltimore$fips )
+NEI_Baltimore$type <- as.factor( NEI_Baltimore$type )
+NEI_Baltimore$year <- as.factor( NEI_Baltimore$year )
+
+## Split the data.frame on year & fips and then summarize by summing Emissions
+baltimorePM25Summarized1 <- ddply( NEI_Baltimore, 
+                                  .( year, fips ), 
+                                  summarize, 
+                                  Emissions = sum( Emissions ) )
+
+## Create a data.frame relating fips code to county name
+fipsCounty <- data.frame( fips = c( '06037', '24510' ), 
+                          county = c( 'Los Angeles County', 'Baltimore City' ) )
+
+## Merge county and Baltimore data.frames
+baltimorePM25Summarized <- merge( 
+                                    baltimorePM25Summarized1, 
+                                    fipsCounty, 
+                                    sort = FALSE )
+
+## Open 'PNG' file device to create image to export to file
+png( 
+      file = "plot6.png",     ## 'PNG' file name
+      width = 480, 
+      height = 480)           
+
+
+g <- qplot( 
+            year, 
+            Emissions,                           ## Scaling factor
+            data = baltimorePM25Summarized, 
+            stat = 'identity', 
+            geom = 'bar',                        ## Bar plot
+            fill = county, 
+            position = 'dodge' ) + 
+      
+      labs(
+            x = expression( 
+                        bold( 'Years' ) ) ) +         ## X-axis label
+      
+      labs(
+            y = expression(                     ## Y-axis label
+                        PM[2.5]*' Emissions '*bolditalic( '( tons )' ) ) ) + 
+      
+      labs(
+            fill = expression(
+                        bold( 'U.S. County') ) ) + ## Legend label
+      
+      ggtitle(
+            expression( 
+                  atop( 
+                        bold( 'Total PM'[2.5]*' Emissions'),   ## Main title
+                        atop( 
+                              bold( '(Motor Vehicle)') ) ) ) ) ## Sub title
+
+print( g )
+
+dev.off()                                           ## Close PNG file device
